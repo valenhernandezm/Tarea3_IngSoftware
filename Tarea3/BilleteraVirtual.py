5f09c4a6e39d62078ea2a463253445746fde3b38
valentina <valentina@localhost>
2016-05-12 01:56:14 -0430

    pruebaUnitaria3 El metodo Saldo existe

--- Tarea3/BilleteraVirtual.py
+++ Tarea3/BilleteraVirtual.py
@@ -5,7 +5,16 @@ Created on May 12, 2016
 '''
 
 class BilleteraElectronica(object):
-    def __init__(self,ID):
+    def __init__(self,ID, nombres, apellidos, CI, PIN):
         self.ID = ID 
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.CI = CI
+        self.PIN = PIN 
+        self.balance = 0
+        
+    def Saldo(self):
+        return self.balance
+     
     
     
--- Tarea3/pruebas.py
+++ Tarea3/pruebas.py
@@ -9,11 +9,23 @@ from BilleteraVirtual import *
 
 class TestBilletera(unittest.TestCase):
     
-    def testAtributoIDcreado(self):
-        b1 = BilleteraElectronica('456')
+    '''def testAtributoIDcreado(self):
+        b1 = BilleteraElectronica('456')'''
         
-    def testAtributosDueno(self):
-        b2 = BilleteraElectronica('456','Valentina','Hernandez', '21534334')
+    '''def testAtributosDueno(self):
+        b2 = BilleteraElectronica('456','Valentina','Hernandez', '21534334')'''
+    
+    def testMetodoSaldo(self):
+        b3 = BilleteraElectronica('456','Valentina','Hernandez', '21534334', 123)
+        b3.Saldo()
+
+if __name__ == '__main__':
+    unittest.main()
+    
+     
+    
+        
+    
         
         
         
