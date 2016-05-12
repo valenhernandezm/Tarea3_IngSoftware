0aacf09b58ded4a814c9c711a2a5b7f31c53184f
giuli <giuli@giuli-VIT-P3400>
2016-05-12 15:44:26 -0430

    Segunda prueba de malicia

--- Tarea3/PruebasGiu.py
+++ Tarea3/PruebasGiu.py
@@ -12,5 +12,10 @@ class TestPrograma(unittest.TestCase):
         def testRecargaNegative(self):
             Billetera = BilleteraElectronica("42Sbb90j" , "Luis", "Pacheco", "8542847", "450183")
             self.assertRaises(Exception, Billetera.recargar, -5000, "HOk234t1" )
+            
+        def testConsumoInvalid(self):
+            Billetera = BilleteraElectronica("42Sbb90j" , "Luis", "Pacheco", "8542847", "450183")
+            Billetera.recargar(100, "HOj234t1")
+            self.assertRaises(Exception, Billetera.consumir, 500, "HOj234t1", 450183)
         
         
