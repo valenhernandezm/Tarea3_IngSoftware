0cd410f3da3ae1a323d4bc9933f99649ac2fce7e
giuli <giuli@giuli-VIT-P3400>
2016-05-12 15:40:49 -0430

    Primera prueba de malicia

---
+++ Tarea3/PruebasGiu.py
@@ -0,0 +1,16 @@
+'''
+Created on 12 de may. de 2016
+
+@author: giuli
+'''
+import unittest
+from BilleteraVirtual import *
+from datetime import datetime
+
+class TestPrograma(unittest.TestCase):
+    
+        def testRecargaNegative(self):
+            Billetera = BilleteraElectronica("42Sbb90j" , "Luis", "Pacheco", "8542847", "450183")
+            self.assertRaises(Exception, Billetera.recargar, -5000, "HOk234t1" )
+        
+        
